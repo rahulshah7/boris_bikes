7bfa495d6a47aa70504052e521db243eba77ed5e
Rahul Shah <rahulshah7@outlook.com>
2019-10-30 16:14:07 +0000

    Refactor respond to test to use one-liner syntax

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,7 +3,5 @@
 require 'docking_station'
 
 describe DockingStation do
-  it 'responds hello to release_bike' do
-    expect(subject).to respond_to(:release_bike)
-  end
+  it { expect(subject).to respond_to(:release_bike) }
 end
