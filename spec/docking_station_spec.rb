c41df8665c9d12a12c217b994a94235f02efc8f9
Rahul Shah <rahulshah7@outlook.com>
2019-11-05 00:43:54 +0000

    Test to ensure #release_bike returns an instance of Bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,4 +4,10 @@ require 'docking_station'
 
 describe DockingStation do
   it { expect(subject).to respond_to(:release_bike) }
+
+  describe '#release_bike' do
+    it 'returns an instance of a Bike' do
+      expect(subject.release_bike).to be_an_instance_of(Bike)
+    end
+  end
 end
