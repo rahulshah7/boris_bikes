42ca4f8bc2e8263d73e109412725dab1ed918752
Rahul Shah <rahulshah7@outlook.com>
2019-11-05 10:24:21 +0000

    Move release bike responsd to test withing describe block

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,9 +3,8 @@
 require 'docking_station'
 
 describe DockingStation do
-  it { expect(subject).to respond_to(:release_bike) }
-
   describe '#release_bike' do
+    it { expect(subject).to respond_to(:release_bike) }
     it 'returns an instance of a Bike' do
       expect(subject.release_bike).to be_an_instance_of(Bike)
     end
